206db6439da3169481d466952c3e753511436fa2
Mehrdad Karami Jourabi <108220416+mehrdad-kj@users.noreply.github.com>
2022-07-16 18:41:15 +0430

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# To-Do-List-Project
+
+This is a ToDo list project with a Digital Clock. I did this to practice JS and Bootstrap.
---
+++ app.js
@@ -0,0 +1,94 @@
+const addForm = document.querySelector('.add');
+const search = document.querySelector('.search input');
+const list = document.querySelector('.todos');
+
+const generateTemplate = todo => {
+  const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span>${todo}</span>
+      <i class="far fa-trash-alt delete"></i>
+    </li>
+  `;
+  list.innerHTML += html;
+};
+
+const filterTodos = term => {
+
+  // add filtered class
+  Array.from(list.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(term))
+    .forEach(todo => todo.classList.add('filtered'));
+
+  // remove filtered class
+  Array.from(list.children)
+    .filter(todo => todo.textContent.toLowerCase().includes(term))
+    .forEach(todo => todo.classList.remove('filtered'));
+
+};
+
+// add todos event
+addForm.addEventListener('submit', e => {
+  
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+
+  if(todo.length){
+    generateTemplate(todo);
+    addForm.reset();
+  }
+
+});
+
+// delete todos event
+list.addEventListener('click', e => {
+
+  if(e.target.classList.contains('delete')){
+    e.target.parentElement.remove();
+  }
+
+});
+
+// filter todos event
+search.addEventListener('keyup', () => {
+
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
+
+});
+
+
+
+
+
+
+
+//making a digital clock
+const clock = document.querySelector('.clock');
+
+
+
+const getTime = ()=>{
+    const time = new Date();
+    const hour = time.getHours();
+    const min = time.getMinutes();
+    const secs = time.getSeconds();
+    
+   if(secs < 10){
+       let html = `
+        <span>${hour}: </span>
+        <span>${min}: </span>
+        <span>0${secs} </span>
+       `;
+       clock.innerHTML = html;
+   } else{
+        let html = `
+            <span>${hour}: </span>
+            <span>${min}: </span>
+            <span>${secs} </span>
+        `;
+       clock.innerHTML = html;
+   }
+  
+};
+
+setInterval(getTime, 1000);
